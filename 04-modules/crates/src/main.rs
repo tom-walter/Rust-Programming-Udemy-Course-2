9421271219c2fb269e76e6249a37bbaeb0c06cc1
Tom Walter <tom.k.walter@gmail.com>
2023-08-23 16:14:47 +0200

    Add detailed code example for Section 4

--- 04-modules/crates/src/main.rs
+++ 04-modules/crates/src/main.rs
@@ -1,8 +1,18 @@
 extern crate rand;
 use rand::Rng;
+use rand::distributions::Alphanumeric; 
 
 fn main() {
     let mut rng = rand::thread_rng();
     let num: i32 = rng.gen();
     println!("random number {}", num);  
+
+    println!("bounded int: {}", rng.gen_range(0, 100));
+    println!("bounded float: {}", rng.gen_range(0.0, 100.0));
+
+    let rand_string: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(20)
+        .collect();
+    println!("Gen string: {}", rand_string); 
 }
